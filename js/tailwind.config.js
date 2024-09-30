2718773cfb0349d1fcd83d8720fa7da38cca7a40
shanto <shantohasibulhasan379@gmail.com>
2024-09-30 22:32:11 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,42 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          background: '#160C6D',
+          hoverColor: '#1BBF00',
+          overly: 'rgba(106, 98, 197, 0.9)',
+          boxshadwo: '#C4C4C4',
+          counterbg: '#F9F9FB',
+          color: '#726E9E',
+          redPart: '#FDFDFD',
+          bluePart: '#141135',
+          boxColor: '#6B62C5',
+          pColor: '#E6E5F3',
+          bgColor: '#F6F5FF',
+          gray: '#6001D3',
+          iconColor: '#FF7628',
+          sescolor: '#221A2C',
+          hedcolor: '#030314',
+          endbg: '#48409C',
+          endcolor: '#FEFEFE',
+        },
+        // container 
+        spacing: {
+            'headerContainer': '1170px',
+            'bannerContainer': '962px',
+          },
+        //   container End 
+        backgroundImage: {
+            'bgImg': "url('images/image.jpg')",
+          },
+          fontFamily: {
+            'open': ['Open Sans',],
+            'papri': ['Paprika',],
+            'nan': ['Nunito',],
+            'quick': ['Quicksand',],
+            'nuni': ['Nunito',],
+
+          },
+      }
+    }
+  }
